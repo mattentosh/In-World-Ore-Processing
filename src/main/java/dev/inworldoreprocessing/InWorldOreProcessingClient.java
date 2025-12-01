0045119155fe83448b4d100b29d42f763c9e186c
mattentosh <mattentosh1@gmail.com>
2025-12-01 14:37:00 +0800

    Initial commit

---
+++ src/main/java/dev/inworldoreprocessing/InWorldOreProcessingClient.java
@@ -0,0 +1,10 @@
+package dev.inworldoreprocessing;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class InWorldOreProcessingClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
